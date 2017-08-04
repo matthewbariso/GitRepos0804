985a913e52c249c5ca96f6b3df32f7e123a405e3
BARISJA2 <BARISJA2@ZHA-ITA016-W7.corp.oocl.com>
2017-08-04 14:02:02 +0800

    COMMIT

--- GitProject/src/com/oocl/training/git/Git0804.java
+++ GitProject/src/com/oocl/training/git/Git0804.java
@@ -4,6 +4,9 @@ public class Git0804 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
+		
+		
+		
 		System.out.println("I have added a second branch");
 		System.out.println("change for second branch");
 	}
