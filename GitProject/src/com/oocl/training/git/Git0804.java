ded2e06e8c28fab65114a04a563852093062de2c
BARISJA2 <BARISJA2@ZHA-ITA016-W7.corp.oocl.com>
2017-08-04 13:46:56 +0800

    fourth push for second branch

--- GitProject/src/com/oocl/training/git/Git0804.java
+++ GitProject/src/com/oocl/training/git/Git0804.java
@@ -4,11 +4,7 @@ public class Git0804 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
-<<<<<<< HEAD
-		System.out.println("This is the third branch");
-=======
 		System.out.println("I have added a second branch");
->>>>>>> refs/heads/master
 		
 	}
 }
